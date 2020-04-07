4866041a6ba227248c9ad4288690167ddd16f35c
Víctor Xirau Guardans <victor.xirau@students.salle.url.edu>
2020-04-07 16:57:20 +0200

    config

---
+++ Client/src/MainClient.java
@@ -0,0 +1,7 @@
+package src;
+
+public class MainClient{
+    public static void main(String[] args) {
+        System.out.println("prova");
+    }
+}
---
+++ Server/src/MainServer.java
@@ -0,0 +1,7 @@
+package src;
+
+public class MainServer {
+    public static void main (String[] args) {
+
+    }
+}
