c309959098494d9c9eaa45f6b7f7b03e54dbbca0
lidiafigueras <lidia.figueras@students.salle.url.edu>
2020-04-15 19:20:55 +0200

    LoginViewController

--- Client/src/View/LoginView.java
+++ Client/src/View/LoginView.java
@@ -14,6 +14,7 @@ public class LoginView extends JFrame implements ActionListener {
     private JTextField jtfpassword;
     private JPanel jpLoginView;
     private JButton botoInici;
+    private JButton jbRegistrat;
 
     public LoginView(){
         //setBackground(Color.RED);
@@ -105,22 +106,24 @@ public class LoginView extends JFrame implements ActionListener {
         jpPare.add(jlText4);
 
         //Text: registrat
-        JLabel jlText5 = new JLabel();
-        jlText5.setText("Registra't ara");
-        jlText5.setBounds(205, 440, 200, 80);
-        jlText5.setOpaque(false);
-        jlText5.setForeground(Color.WHITE);
-        jlText5.setFont(new Font("Helvetica", Font.BOLD, 15));
-        jpPare.add(jlText5);
+        jbRegistrat = new JButton();
+        jbRegistrat.setText("Registra't ara");
+        jbRegistrat.setBounds(150, 440, 200, 80);
+        jbRegistrat.setOpaque(false);
+        jbRegistrat.setForeground(Color.WHITE);
+        jbRegistrat.setContentAreaFilled(false);
+        jbRegistrat.setBorderPainted(false);
+        jbRegistrat.setFont(new Font("Helvetica", Font.BOLD, 15));
+        jpPare.add(jbRegistrat);
+
 
         //Boto inici sessio
-        JButton botoInici = new JButton();
+        botoInici = new JButton();
         botoInici.setText("INICIAR SESSIÓ");
         botoInici.setEnabled(true);
         botoInici.setBounds(130, 600, 200, 40);
         botoInici.setFont(new Font("Herculanum", Font.BOLD, 20));
         botoInici.setBackground(Color.decode("#FEAB24"));
-        botoInici.setForeground(Color.decode("#FEAB24"));
         botoInici.setOpaque(true);
         jpPare.add(botoInici);
 
@@ -155,89 +158,10 @@ public class LoginView extends JFrame implements ActionListener {
         return jtfpassword.getText();
     }
 
-
-    /*private void createPanels(){
-        JPanel jpPare = new JPanel();
-        jpPare.setLayout(null);
-        jpPare.setOpaque(false);
-
-        //Títol: AGE ROYALE
-        JLabel jlTitol = new JLabel();
-        jlTitol.setText("AGE ROYALE");
-        jlTitol.setBounds(60, 60, 300, 50);
-        jlTitol.setHorizontalAlignment(SwingConstants.CENTER);
-        jlTitol.setFont(new Font("Herculanum", Font.BOLD, 40));
-        jlTitol.setOpaque(false);
-        jpPare.add(jlTitol);
-
-        //Icona
-        ImageIcon imagen = new ImageIcon("/resources/escut.png");
-        JLabel jlIcon = new JLabel(imagen);
-        //jpAuxLogo.add(new JLabel(new ImageIcon(getClass().getResource("/resources/escut.png"))));
-        jlIcon.setBounds(10, 80, 50, 50);
-        jpPare.add(jlIcon);
-
-
-        this.setContentPane(jpPare);
-        /*JPanel panelPare = new JPanel();
-        panelPare.setLayout(new GridLayout(9,1));
-        panelPare.setOpaque(false);
-
-        JPanel margen[] = new JPanel[15];
-        for(int i=0; i<15 ;i++){
-            margen[i] = new JPanel();
-            margen[i].setOpaque(false);
-        }
-
-        JPanel jpAuxT = new JPanel(new BorderLayout());
-        JLabel titol = new JLabel("<html><font color='white'>Age Royale</font></html>");
-        titol.setHorizontalAlignment(JLabel.CENTER);
-        jpAuxT.add(titol, BorderLayout.CENTER);
-        jpAuxT.setOpaque(false);
-
-        JPanel jpAuxLogo = new JPanel();
-        jpAuxLogo.add(new JLabel(new ImageIcon(getClass().getResource("/resources/escut.png"))));
-        jpAuxLogo.setOpaque(false);
-        jpAuxLogo.setPreferredSize(new Dimension(50,50));
-
-        JPanel jpAux1 = new JPanel(new FlowLayout());
-        jpAux1.add(new JLabel("<html><font color='white'>"+ Utils.ferEspais(10) +" Nom d'usuari:"+ Utils.ferEspais(2) +"</font></html>"), BorderLayout.LINE_START);
-        jtfusername = new JTextField();
-        jtfusername.setColumns(10);
-        jpAux1.add(jtfusername);
-        jpAux1.setOpaque(false);
-
-
-        JPanel jpAux2 = new JPanel(new FlowLayout());
-        jpAux2.add(new JLabel("<html><font color='white'>"+ Utils.ferEspais(21) +" Correu:"+ Utils.ferEspais(2) +"</font></html>"), BorderLayout.LINE_START);
-        jtfpassword = new JTextField();
-        jtfpassword.setColumns(10);
-        jpAux2.add(jtfpassword);
-        jpAux2.setOpaque(false);
-
-        JPanel jpAux4 = new JPanel(new GridLayout(3,3));
-        jpAux4.add(margen[1]);
-        jpAux4.add(margen[2]);
-        jpAux4.add(margen[3]);
-        jpAux4.add(margen[4]);
-        botoInici = new JButton("Inicia sessió");
-        jpAux4.add(botoInici);
-        jpAux4.add(margen[5]);
-        jpAux4.add(margen[6]);
-        jpAux4.add(margen[7]);
-        jpAux4.add(margen[8]);
-        jpAux4.setOpaque(false);
-
-
-        panelPare.add(jpAuxT);
-        panelPare.add(jpAuxLogo);
-        panelPare.add(margen[0]);
-        panelPare.add(jpAux1, BorderLayout.WEST);
-        panelPare.add(jpAux1);
-        panelPare.add(jpAux2);
-        panelPare.add(jpAux4);
-
-        this.setContentPane(panelPare);*/
+    public void loginViewsetListener(ActionListener controlador) {
+        botoInici.addActionListener(controlador);
+        jbRegistrat.addActionListener(controlador);
+    }
 
 
 }
